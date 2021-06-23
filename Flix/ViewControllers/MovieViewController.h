b83a77c5f00c922dc3a45ff26bfc6e2e2f2007ef
Sebastian Saldana Cardenas <sebastiansalc@fb.com>
2021-06-23 12:12:44 -0500

    Adds network call to bring data from movie API and saves into a movie array property

---
+++ Flix/ViewControllers/MovieViewController.h
@@ -0,0 +1,16 @@
+//
+//  MovieViewController.h
+//  Flix
+//
+//  Created by Sebastian Saldana Cardenas on 23/06/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
