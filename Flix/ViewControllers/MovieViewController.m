5dea1474781df37ddb6301d72338cc05c1badc46
Sebastian Saldana Cardenas <sebastiansalc@fb.com>
2021-06-24 18:17:33 -0500

    Adds collection view using same movies

--- Flix/ViewControllers/MovieViewController.m
+++ Flix/ViewControllers/MovieViewController.m
@@ -24,7 +24,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
+        
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     
---
+++ Flix/Views/MovieCollectionViewCell.m
@@ -0,0 +1,12 @@
+//
+//  MovieCollectionViewCell.m
+//  Flix
+//
+//  Created by Sebastian Saldana Cardenas on 24/06/21.
+//
+
+#import "MovieCollectionViewCell.h"
+
+@implementation MovieCollectionViewCell
+
+@end
